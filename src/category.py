3cd27eb517e161b38eb43fdb66270ef4ec6998ab
natalia-yep_msi <epif97@mail.ru>
2025-01-11 11:13:08 +0300

    add getter and add_product for Category

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from tests.conftest import product
+
+
 class Category:
     """Класс для описания категории продуктов"""
 
@@ -11,6 +14,19 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, new_product):
+        self.__products.append(new_product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_str = ""
+        for element in self.__products:
+            product_str += f'{element.name}, {element.price} руб. Остаток: {element.quantity} шт.\n'
+        return product_str
+
+
