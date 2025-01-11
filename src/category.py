66ea1a78ef1d4ab6acc161addbf55a7afe1eda23
natalia-yep_msi <epif97@mail.ru>
2025-01-11 16:13:28 +0300

    add tests for category

--- src/category.py
+++ src/category.py
@@ -10,11 +10,13 @@ class Category:
     category_count = 0
     product_count = 0
 
+
     def __init__(self, name: str, description: str, products: list) -> None:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.__products = products
+        Category.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
 
@@ -24,9 +26,14 @@ class Category:
         Category.product_count += 1
 
     @property
+    def products_in_list(self) -> list:
+        return self.__products
+
+    @property
     def products(self) -> str:
         """Метод-геттер для получения строки, содержащей информацию о продуктах категории"""
         product_str = ""
         for element in self.__products:
             product_str += f"{element.name}, {element.price} руб. Остаток: {element.quantity} шт.\n"
         return product_str
+
